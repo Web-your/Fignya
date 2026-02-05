36a0491475d1e39b43f4bdc8cd7efe1907c09902
IvanZhiltsov <ivanzh08@gmail.com>
2026-02-05 19:00:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,32 @@ class MapApplication(QMainWindow):
         uic.loadUi('MainWindow.ui', self)
         self.showMaximized()
 
+        self.is_light_mode = True
+
+        self.modes = [1, 2, 3]
+        self.curr_mode_index = 0
+
+        self.center_x = 0
+        self.center_y = 0
+        self.move_step = 0.0001
+        self.min_cords = -180
+        self.max_cords = 180
+
+        self.scale = 1
+        self.scale_step = 0.1
+        self.min_scale = 0.1
+        self.max_scale = 3
+
+        self.search_button.clicked.connect(self.search_cords)
+        self.search_address_button.clicked.connect(self.search_address)
+        self.theme_button.clicked.connect(self.change_theme)
+        self.mode_button.clicked.connect(self.change_mode)
+        self.hui_button.clicked.connect(self.do_something)
+
     def update(self):
-        ...
+        print(self.is_light_mode)
+        print(self.curr_mode_index, self.modes[self.curr_mode_index])
+        print(self.center_x, self.center_y)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key.Key_PageUp:
@@ -52,10 +76,43 @@ class MapApplication(QMainWindow):
         if event.key() == Qt.Key.Key_Left:
             ...
 
+    def search_cords(self):
+        x_text = self.longitube_edit.text()
+        y_text = (self.lattitube_edit.text())
+
+        if x_text:
+            try:
+                self.center_x = float(x_text)
+            except ValueError:
+                print("Координаты имеют формат float")
+
+        if y_text:
+            try:
+                self.center_x = float(y_text)
+            except ValueError:
+                print("Координаты имеют формат float")
+
+        self.update()
+
+    def search_address(self):
+        ...
+
+    def change_theme(self):
+        self.is_light_mode = not self.is_light_mode
+        self.update()
+
+    def change_mode(self):
+        self.curr_mode_index += 1
+        if self.curr_mode_index >= len(self.modes):
+            self.curr_mode_index = 0
+        self.update()
+
+    def do_something(self):
+        ...
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     ex = MapApplication()
     ex.show()
     sys.excepthook = except_hook
